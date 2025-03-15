95d187257d0243c0e735ba73a87289e9a6cd6c0f
laeka <ale.tct16@gmail.com>
2025-03-15 13:00:32 -0500

    no more loop, sending random emails with github actions

--- cjemail.py
+++ cjemail.py
@@ -3,37 +3,22 @@ import logging
 import signal
 import sys
 import smtplib
-import time
-import itertools
-import http.server
-import socketserver
+import random
 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
 
-# Configurar el logger
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
-
-load_dotenv()
-logging.info("Environment variables loaded successfully")
-
 
 def send_email(sender, recipients, subject, body, smtp_server, port, user, password):
     logging.info(f"Attempting to send email from {sender} to {', '.join(recipients)}")
-    # Configure the message
     message = MIMEMultipart()
     message["From"] = sender
-    message["To"] = ", ".join(
-        recipients
-    )  # Join recipients into a comma-separated string
+    message["To"] = ", ".join(recipients)
     message["Subject"] = subject
     message.attach(MIMEText(body, "plain"))
 
     try:
-        # Send the email
         with smtplib.SMTP(smtp_server, port) as server:
             server.starttls()
             server.login(user, password)
@@ -50,93 +35,80 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 
-signal.signal(signal.SIGINT, signal_handler)
-signal.signal(signal.SIGTERM, signal_handler)
+def main():
+    # Configurar manejadores de signal par una terminacion controlada
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
 
-EMAIL_PORT = 587
+    # Configurar el logger y cargar las variables de entorno
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+    load_dotenv()
+    logging.info("Variables de entorno cargadas exitosamente")
 
-# List of email configurations
-email_configurations = [
-    {
-        "smtp_server": "smtp.gmail.com",
-        "port": EMAIL_PORT,
-        "user": os.getenv("EMAIL_USER_1"),
-        "password": os.getenv("EMAIL_PASSWORD_1"),
-    },
-    {
-        "smtp_server": "smtp.gmail.com",
-        "port": EMAIL_PORT,
-        "user": os.getenv("EMAIL_USER_2"),
-        "password": os.getenv("EMAIL_PASSWORD_2"),
-    },
-    {
-        "smtp_server": "smtp.gmail.com",
-        "port": EMAIL_PORT,
-        "user": os.getenv("EMAIL_USER_3"),
-        "password": os.getenv("EMAIL_PASSWORD_3"),
-    },
-    {
-        "smtp_server": "smtp.gmail.com",
-        "port": EMAIL_PORT,
-        "user": os.getenv("EMAIL_USER_4"),
-        "password": os.getenv("EMAIL_PASSWORD_4"),
-    },
-    {
-        "smtp_server": "smtp.gmail.com",
-        "port": EMAIL_PORT,
-        "user": os.getenv("EMAIL_USER_5"),
-        "password": os.getenv("EMAIL_PASSWORD_5"),
-    },
-]
+    EMAIL_PORT = 587
 
-# List of recipients
-recipients = [
-    "contacto@kobsa.com.pe",
-    "archivos@kobsa.com.pe",
-    "abarrionuevo@kobsa.com.pe",
-    "avera@kobsa.com.pe",
-    "gestion.personal@kobsa.com.pe",
-]
+    email_configurations = [
+        {
+            "smtp_server": "smtp.gmail.com",
+            "port": EMAIL_PORT,
+            "user": os.getenv("EMAIL_USER_1"),
+            "password": os.getenv("EMAIL_PASSWORD_1"),
+        },
+        {
+            "smtp_server": "smtp.gmail.com",
+            "port": EMAIL_PORT,
+            "user": os.getenv("EMAIL_USER_2"),
+            "password": os.getenv("EMAIL_PASSWORD_2"),
+        },
+        {
+            "smtp_server": "smtp.gmail.com",
+            "port": EMAIL_PORT,
+            "user": os.getenv("EMAIL_USER_3"),
+            "password": os.getenv("EMAIL_PASSWORD_3"),
+        },
+        {
+            "smtp_server": "smtp.gmail.com",
+            "port": EMAIL_PORT,
+            "user": os.getenv("EMAIL_USER_4"),
+            "password": os.getenv("EMAIL_PASSWORD_4"),
+        },
+        {
+            "smtp_server": "smtp.gmail.com",
+            "port": EMAIL_PORT,
+            "user": os.getenv("EMAIL_USER_5"),
+            "password": os.getenv("EMAIL_PASSWORD_5"),
+        },
+    ]
 
-# Email data to send
-subject = "Pago de mi liquidacion atrasado 1 año y 6 meses"
-body = (
-    "Por favor pagar lo restante de mi liquidacion mi dni es: 72767027. "
-    "Se los suplico lo necesito urgente, mi numero de cuenta bbva es: 0011-0814-0260747032 - "
-    "A pesar del mensaje enviado por whatsapp necesito una fecha exacta ya que es de suma urgencia y creo que es lo correcto"
-)
+    recipients = [
+        "contacto@kobsa.com.pe",
+        "archivos@kobsa.com.pe",
+        "abarrionuevo@kobsa.com.pe",
+        "avera@kobsa.com.pe",
+        "gestion.personal@kobsa.com.pe",
+    ]
+
+    subject = "Pago de mi liquidacion atrasado 1 año y 6 meses"
+    body = (
+        "Por favor pagar lo restante de mi liquidacion mi dni es: 72767027. "
+        "Se los suplico lo necesito urgente, mi numero de cuenta bbva es: 0011-0814-0260747032 - "
+        "Sigo sin tener respuesta y por whatsapp ya no me contestan ya pasaron: 1 año y 3 meses por favor!!"
+    )
+
+    config = random.choice(email_configurations)
+    send_email(
+        sender=config["user"],
+        recipients=recipients,
+        subject=subject,
+        body=body,
+        smtp_server=config["smtp_server"],
+        port=config["port"],
+        user=config["user"],
+        password=config["password"],
+    )
 
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"Server is running")
-
-
-port = int(os.environ.get("PORT", 8080))
-logging.info(f"Starting server on port {port}")
-
-with socketserver.TCPServer(("", port), Handler) as httpd:
-    logging.info(f"Serving on port {port}")
-    import threading
-
-    server_thread = threading.Thread(target=httpd.serve_forever)
-    server_thread.daemon = True
-    server_thread.start()
-
-    # Rotate between email accounts
-    for config in itertools.cycle(email_configurations):
-        send_email(
-            sender=config["user"],
-            recipients=recipients,
-            subject=subject,
-            body=body,
-            smtp_server=config["smtp_server"],
-            port=config["port"],
-            user=config["user"],
-            password=config["password"],
-        )
-        # Wait 5 minutes before sending the next email
-        logging.info(f"Waiting for 5 minutes before sending the next email...")
-        time.sleep(300)
+if __name__ == "__main__":
+    main()
